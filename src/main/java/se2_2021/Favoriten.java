f722891220db65813299dee286879f969f7d61aa
Nico <Nico@192.168.0.143>
2021-12-17 12:20:29 +0100

    Klasse Fav

--- src/main/java/se2_2021/Favoriten.java
+++ src/main/java/se2_2021/Favoriten.java
@@ -1,5 +1,39 @@
 package se2_2021;
 
+import java.util.ArrayList;
+
 public class Favoriten {
 
+	private ArrayList<String> favoriten;
+	
+	
+	public void favouriteHinzufügen(String name, String adresse) {
+		String n = name + " : " + adresse;
+		favoriten.add(n);
+	}
+	
+	
+	public void favoriteEntfernen(String name, String adresse) {
+		String n = name + " : " + adresse;
+		for (String str : favoriten) {
+			if (n == str) {
+				favoriten.remove(str);
+			}
+		}
+	}
+	
+	public ArrayList<String> getFavoriten() {
+		return favoriten;
+	}
+	
+	
+	public void favoritenAnzeigen() {
+		for (String str : favoriten) {
+			System.out.print(str);
+		}
+	}
+	
+	
+	
+	
 }
