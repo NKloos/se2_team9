c09a397a884e43255fa9cce8912430832c05977a
Nico <Nico@192.168.0.143>
2021-12-17 13:42:47 +0100

    2 fertige funtionierende tests

--- src/test/java/se2_2021/TestFavoriten.java
+++ src/test/java/se2_2021/TestFavoriten.java
@@ -13,9 +13,22 @@ class testFavoriten {
 	@Test
 	void testHinzufüegen() {
 		Favoriten f = new Favoriten();
-		f.favoriteHinzufügen("Herman", "Walter");
+		f.favoriteHinzufügen("Zuhause", "Offenbergerstr. 28");
 		assertEquals(1,f.getFavoriten().size());
-		assertEquals("Herman : Walter",f.getFavoriten().get(0));
+		assertEquals("Zuhause : Offenbergerstr. 28",f.getFavoriten().get(0));
+		
+	}
+	
+	@Test
+	void testEntfernen() {
+		Favoriten f = new Favoriten();
+		f.favoriteHinzufügen("Mutter", "Weinberstr. 6");
+		f.favoriteHinzufügen("Zuhause", "Offenbergerstr. 28");
+		assertEquals(2,f.getFavoriten().size());
+		f.favoriteEntfernen("Mutter", "Weinberstr. 6");
+		assertEquals("Zuhause : Offenbergerstr. 28",f.getFavoriten().get(0));
+		assertEquals(1,f.getFavoriten().size());
+		
 		
 	}
 	
